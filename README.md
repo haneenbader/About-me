0585f8c1ca4647f637a7d77ffe38b5b037dd348e
haneenbader <haneenalwatan993@gmail.com>
2021-04-06 16:41:39 +0300

    solve lab03

--- README.md
+++ README.md
@@ -4,5 +4,10 @@
 ##**About-me** project
 
 This webpage build to show some information about me  and  my page for handmadecraft .
+i mintioned ten color i love it 
+i ask the user to guss number , enter it and alert the tru number i want 
+i ask the user to enter the color i love it and check if the color is true or false 
+i added the score to the correct user answer
+
 
 Author: Haneen bader
--- js/app.js
+++ js/app.js
@@ -1,59 +1,64 @@
 'use strict'
+let score = 0 ;
 let userName = prompt('what is your name ?');
-alert ('welcom ' + userName + ' in my webpage');
+alert('welcom ' + userName + ' in my webpage');
 
-let b = prompt('are you happy')
+let b = prompt('do you think im happy ?')
 //console.log(b);
 b = b.toUpperCase();
-switch(b){
-    case('YES'):
-    alert('you are happy');
-    break;
-    case('NO'):
-    alert('you are sad');
-    break;
+switch (b) {
+    case ('YES'):
+        alert('im happy');
+        score++;
+        break;
+    case ('NO'):
+        alert('wrong , im happy');
+        break;
     default:
         alert('ok no problem');
 }
 
-let c = prompt('your age is greater than 20 ')
+let c = prompt('my  age is greater than 20 ? ')
 //console.log(c);
 c = c.toLowerCase();
-switch(c){
-    case('yes'):
-    alert('your age is allowed in this page');
-    break;
-    case('no'):
-    alert('ok no problem ');
-    break;
+switch (c) {
+    case ('yes'):
+        alert('my age is 27 years');
+        score++;
+        break;
+    case ('no'):
+        alert('ok , im 27 years old ');
+        break;
     default:
         alert('welcome');
 }
 
-let d = prompt('are you live jordan? ')
+let d = prompt('i live  in jordan? ')
 //console.log(d);
 d = d.toLowerCase();
-switch(d){
-    case('yes'):
-    alert('your live in jordan');
-    break;
-    case('no'):
-    alert('your live out jordan ');
-    break;
+switch (d) {
+    case ('yes'):
+        alert('great answer');
+        score++;
+        break;
+    case ('no'):
+        alert('i live in jordan ');
+        break;
     default:
         alert('welcome');
 }
 
-let e = prompt('are you love handmadecraft?')
+let e = prompt('do you think im an engineer ?')
 //console.log(e);
 e = e.toLowerCase();
-switch(e){
-    case('yes'):
-    alert('you love hand craft');
-    break;
-    case('no'):
-    alert('you  dont love hand craft');
-    break;
+switch (e) {
+    case ('yes'):
+        alert('yes im an engineer');
+        score++;
+        break;
+    case ('no'):
+        alert('im a civil engineer');
+        break;
     default:
         alert('ok no problem');
 }
@@ -61,16 +66,98 @@ switch(e){
 let a = prompt('do yo think i love handmadecraft?')
 //console.log(a);
 a = a.toLowerCase();
-switch(a){
-    case('yes'):
-    alert('you answer is true');
-    break;
-    case('no'):
-    alert('you answer is false');
-    break;
+switch (a) {
+    case ('yes'):
+        alert('you answer is true');
+        score++;
+        break;
+    case ('no'):
+        alert('you answer is false');
+        break;
     default:
         alert('ok no problem');
 }
 
-alert ('welcom ' + userName ); 
-document.write('welcom ' + userName );
+
+
+
+let guessNmber = prompt(' guess !! what is the  number in my mind ?');
+guessNmber = parseInt(guessNmber);
+for (let i = 1; i < 4; i++) {
+    if (parseInt(guessNmber) === 20) {
+        alert('great , the number is 20 ')
+        score++;
+        break;
+    } else if (parseInt(guessNmber) < 20) {
+        alert('too low')
+        guessNmber = prompt('guess another number greater than this number')
+    } else if (parseInt(guessNmber) > 20) {
+        alert('too high')
+        guessNmber = prompt('guess another number less than this number')
+
+    }
+}
+alert('20 is the correct answer');
+
+
+let myColor = ['red', 'green']
+for (let c=0 ; c<6 ; c++){
+    let userColor = prompt('what do you think  the color i love it ?  ');
+    userColor = userColor.toLowerCase();
+
+    if(userColor ===  myColor[0] || userColor ===  myColor[1] ){
+        alert('its right this is one of my colors');
+        score++;
+        break;
+    }else {
+        alert('its wrong this is not one of my colors'); 
+    }
+}
+alert('me best colors is ' + myColor);
+
+alert('your score is ' + score ) ;
+alert('welcom ' + userName);    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let myColor = ['red', 'green', 'black', 'yellow'];
+
+//for (let x = 1; x <= 6; x++) {
+    
+ //   let userColor = prompt('what do you think  the color i love it ?  ');
+  //  userColor = userColor.toLowerCase();
+  //  for (let n = 0; n < myColor.length; n++) {
+  //      if (userColor === myColor[n]) {
+  //          alert(' its right this is one of my colors');
+   //         break;
+   //     } else if (userColor !== myColor[n]) {
+//
+  //      }
+  //      break;
+  //  }
+  // 
+    //userColor = prompt(' enter another  color do you think i love it ? ');
+    
+
+//}
+
+
+
+//alert('me best colors is ' + myColor);
+
+
+
+
+
